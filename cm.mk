71f4bab06f758d48e22c7aff875e14e813baea27
Pavel Kirpichyov <pavel.kirpichyov@gmail.com>
2012-12-19 17:47:04 +0200

    Update product name

--- cm.mk
+++ cm.mk
@@ -14,7 +14,7 @@ PRODUCT_VERSION_DEVICE_SPECIFIC := -GT-S5830
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := cooper
-PRODUCT_NAME := jellaxy_cooper
+PRODUCT_NAME := cm_cooper
 
 ## Bootanimation
 #TARGET_BOOTANIMATION_NAME := vertical-320x480
