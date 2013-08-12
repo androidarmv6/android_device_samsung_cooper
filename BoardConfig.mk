81177d7f35bd84001b51fdb722fb4aac9ccfc58d
bieltv3 <bieltv.3@gmail.com>
2013-08-12 21:20:28 +0200

    fstab v2 for cooper
    Change-Id: I0bf96d36d10d1abc1e68974841aa400ac1668a43

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,7 +33,8 @@ BOARD_USE_CUSTOM_RECOVERY_FONT := "<font_7x16.h>"
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/cooper/recovery/recovery_ui.c
 BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/cooper/recovery/graphics.c
 TARGET_RECOVERY_INITRC := device/samsung/cooper/recovery/recovery.rc
-TARGET_RECOVERY_FSTAB := device/samsung/cooper/recovery/recovery.fstab
+TARGET_RECOVERY_FSTAB := device/samsung/cooper/recovery/fstab.cooper
+RECOVERY_FSTAB_VERSION := 2
 
 SMALLER_FONT_FOOTPRINT := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/cooper/bluetooth
