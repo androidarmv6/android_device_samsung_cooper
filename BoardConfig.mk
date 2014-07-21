785f5ade53b122357d802bf3ec65ceb592f3fc7b
Conn O'Griofa <connogriofa@gmail.com>
2014-07-21 14:06:56 +0100

    cooper: configure eng build to use recovery kernel config
    Change-Id: I4efc0ca07490c30c657dd468eafe3d191eceef8d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,11 @@ include device/samsung/msm7x27-common/BoardConfigCommon.mk
 
 ## Kernel, bootloader
 TARGET_BOOTLOADER_BOARD_NAME := cooper
-TARGET_KERNEL_CONFIG := cyanogenmod_cooper_defconfig
+ifneq (eng,$(TARGET_BUILD_VARIANT))
+	TARGET_KERNEL_CONFIG := cyanogenmod_cooper_defconfig
+else
+	TARGET_KERNEL_CONFIG := cyanogenmod_cooper_recovery_defconfig
+endif
 TARGET_OTA_ASSERT_DEVICE := cooper,GT-S5830
 
 ## Recovery
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,6 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-
-add_lunch_combo cm_cooper-userdebug
+for i in eng userdebug; do
+    add_lunch_combo cm_cooper-${i}
+done
